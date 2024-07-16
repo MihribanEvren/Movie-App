5a593b6bfd3c475cda36491aaae2d3321bffcf1c
Mihriban Evren <mihribanevrenn@gmail.com>
2024-07-16 13:29:58 +0300

    feat: update routes

--- src/router.jsx
+++ src/router.jsx
@@ -5,11 +5,10 @@ import TVSeries from './pages/TVSeries';
 import ErrorPage from './pages/ErrorPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Container } from '@mui/material';
 
 function router() {
   return (
-    <Container maxWidth="lg">
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +17,7 @@ function router() {
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
-    </Container>
+    </>
   );
 }
 
